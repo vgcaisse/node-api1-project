10bd11f623b7f54e52b3c37fda63604a55cfad26
Visnu <vgcaisse@gmail.com>
2022-04-11 21:29:54 -0400

    connected my server to the module exports

--- api/server.js
+++ api/server.js
@@ -1,3 +1,16 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+
+
+// | Method | URL            | Description                                                                                            |
+// | ------ | -------------- | ------------------------------------------------------------------------------------------------------ |
+// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
+// | GET    | /api/users     | Returns an array users.                                                                                |
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
+// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
