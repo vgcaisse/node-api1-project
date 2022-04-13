91ee3fd60b3f774bcff63879d7e6fbfdddac41d9
Visnu <vgcaisse@gmail.com>
2022-04-12 21:00:35 -0400

    finished all the code but the server is broken so idk, i give

--- api/server.js
+++ api/server.js
@@ -1,36 +1,88 @@
+const express = require('express')
+const server = express();
+const User = require('./users/model')
 // BUILD YOUR SERVER HERE
-const express = require('express');
-const User = require('./users/model');
-
-const server = express()
-
-server.get('/api/users', (req, res) => {
-    User.find()
-        .then(users => {
-            throw new Error('its quiet... to quiet')
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'getting users',
-                err: err.message
-            })
-        })
-})
-
-server.use('*', (req, res) => {
-    res.status(404).json({
-        message: 'not really found'
+server.use(express.json());
+server.get('/api/users',(req, res) => {
+User.find()
+.then(users => {
+    res.json(users);
+});
+});
+//GET
+server.get('/api/users/:id', (req, res) => {
+   User.findById(req.params.id)
+    .then(user => {
+        if(user == null){
+            res.status(404).json({message: `does not exist`})
+        }else{
+            res.status(200).json(user)
+        }
+    })
+    .catch(() => {
+        res.status(400).json({message: `Could not get user`})
     })
 })
+//POST
+server.post('/api/users', (req, res) =>{
+    const body = req.body;
+    if(!req.body.name || !req.body.bio) {
+        res.status(400).json({ message:  "Please provide name and bio for the user" });
+        return;
+    }
+  
+
+User.insert(body)
+.then(user =>{
+    res.status(201).json(user);
+})
+.catch(() =>{
+    res.status(500).json({message:"There was an error while saving the user to the database"})
+})
+})
+
+//PUT
+server.put('/api/users/:id', (req, res) => {
+    const user = req.body;
+
+    if(!req.body.name) {
+        res.status(400).json({ message: "Please provide name and bio for the user" });
+        return;
+    } else if(!req.body.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" });
+        return;
+    }
+
+    User.update(req.params.id, user)
+        .then(user => {
+            if(user == null) {
+                res.status(404).json({ message: `The user with the specified ID does not exist`});
+            } else {
+                res.status(200).json(user);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message:"The user information could not be modified" });
+        });
+});
+
+//Delete
+server.delete('/api/users/:id', (req, res) => {
+    
+    User.remove(req.params.id)
+        .then(user => {
+            if(user == null) {
+                res.status(404).json({ message: `does not exist` });
+                
+            } else {res.status(200).json(user);}
+
+            
+        })
+        .catch(() => {
+            res.status(400).json({ message: `could not delete user!` });
+        });
+});
+
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
-
-
-
-// | Method | URL            | Description                                                                                            |
-// | ------ | -------------- | ------------------------------------------------------------------------------------------------------ |
-// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.                                   |
-// | GET    | /api/users     | Returns an array users.                                                                                |
-// | GET    | /api/users/:id | Returns the user object with the specified `id`.                                                       |
-// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.                                 |
-// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified user |
